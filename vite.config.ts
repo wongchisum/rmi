9663151c14649a75107fdcf397f4857451a8cb73
王志森 <im.wongch1sum@gmail.com>
2022-08-16 17:20:31 +0800

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from 'vite'
+import {resolve} from 'path'
+export default defineConfig({
+    build:{
+        lib:{
+            entry:resolve(__dirname,"./src/index.tsx")
+        }
+    }
+})
